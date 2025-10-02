bfa3f979f8ff09a5e136c02ba1f2e18a889cc061
fdacky <72664014+fd4cky@users.noreply.github.com>
2025-10-02 11:05:12 +0500

    3 task

---
+++ Практика 2/3.py
@@ -0,0 +1,15 @@
+count = int(input())
+
+
+def print_pack_report(count: int) -> None:
+    match count:
+        case x if x % 5 == 0 and x % 3 == 0:
+            print("расфасуем по 3 или по 5")
+        case x if x % 5 == 0:
+            print("расфасуем по 5")
+        case x if x % 3 == 0:
+            print("расфасуем по 3")
+        case _:
+            print("не заказываем!")
+    
+print_pack_report(count)
