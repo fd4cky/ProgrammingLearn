25a5d39a32df5f1d3232255ecb2a7b08e9da92dc
fdacky <72664014+fd4cky@users.noreply.github.com>
2025-10-02 10:56:08 +0500

    bug

--- Практика 2/2.py
+++ Практика 2/2.py
@@ -1,12 +1,13 @@
 scores = [20, 48, 52, 38, 36, 13, 7, 41, 34, 24, 5, 51, 9, 14] # test
 student_score = 48
 
-def check_winners(scores: list, student_score: int) -> str:
+def check_winners(scores: list, student_score: int) -> None:
     scores_sorted = sorted(scores, reverse=True)
     student_index = scores_sorted.index(student_score)
 
     if student_index + 1 <= 3:
-        return "Вы в тройке победителей!"
-    return "Вы не попали в тройку победителей."
+        print("Вы в тройке победителей!")
+    else:
+        print("Вы не попали в тройку победителей.")
 
-print(check_winners(scores, student_score))
+check_winners(scores, student_score)
