d6704a94e7b5caa5dd5f9af4686489df4d537f27
fdacky <72664014+fd4cky@users.noreply.github.com>
2025-11-01 17:10:59 +0500

    1 task

--- Практика 6/практика 1/1.py
+++ Практика 6/практика 1/1.py
@@ -1,10 +1,26 @@
 import requests
 
-#response = requests.get('https://pokeapi.co/api/v2/ability/?limit=20&offset=20')
-resp = {'count': 367, 'next': 'https://pokeapi.co/api/v2/ability/?offset=40&limit=20', 'previous': 'https://pokeapi.co/api/v2/ability/?offset=0&limit=20', 'results': [{'name': 'suction-cups', 'url': 'https://pokeapi.co/api/v2/ability/21/'}, {'name': 'intimidate', 'url': 'https://pokeapi.co/api/v2/ability/22/'}, {'name': 'shadow-tag', 'url': 'https://pokeapi.co/api/v2/ability/23/'}, {'name': 'rough-skin', 'url': 'https://pokeapi.co/api/v2/ability/24/'}, {'name': 'wonder-guard', 'url': 'https://pokeapi.co/api/v2/ability/25/'}, {'name': 'levitate', 'url': 'https://pokeapi.co/api/v2/ability/26/'}, {'name': 'effect-spore', 'url': 'https://pokeapi.co/api/v2/ability/27/'}, {'name': 'synchronize', 'url': 'https://pokeapi.co/api/v2/ability/28/'}, {'name': 'clear-body', 'url': 'https://pokeapi.co/api/v2/ability/29/'}, {'name': 'natural-cure', 'url': 'https://pokeapi.co/api/v2/ability/30/'}, {'name': 'lightning-rod', 'url': 'https://pokeapi.co/api/v2/ability/31/'}, {'name': 'serene-grace', 'url': 'https://pokeapi.co/api/v2/ability/32/'}, {'name': 'swift-swim', 'url': 'https://pokeapi.co/api/v2/ability/33/'}, {'name': 'chlorophyll', 'url': 'https://pokeapi.co/api/v2/ability/34/'}, {'name': 'illuminate', 'url': 'https://pokeapi.co/api/v2/ability/35/'}, {'name': 'trace', 'url': 'https://pokeapi.co/api/v2/ability/36/'}, {'name': 'huge-power', 'url': 'https://pokeapi.co/api/v2/ability/37/'}, {'name': 'poison-point', 'url': 'https://pokeapi.co/api/v2/ability/38/'}, {'name': 'inner-focus', 'url': 'https://pokeapi.co/api/v2/ability/39/'}, {'name': 'magma-armor', 'url': 'https://pokeapi.co/api/v2/ability/40/'}]}
-pokemons = resp['results']
-print(' '.join([x['name'] for x in pokemons]))
-name = 'illuminate' #input('Имя покемона: ')
-response2 = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}/')
-array = [name]
-print(response2)
+
+# Из запроса получаем name и id покемона, создаем словарь вида {name: id}
+pokemons = dict()
+response = requests.get('https://pokeapi.co/api/v2/ability/?limit=20&offset=20')
+
+for pokemon in response.json()['results']:
+    pokemons[pokemon['name']] = pokemon['url'].split('/')[-2]
+
+print(' '.join([name for name in pokemons.keys()]))
+
+name = input('\nИмя покемона: ')
+
+# Создаем запрос и вместо name указываем id покемона
+pokemon_info = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemons[name]}/').json()
+
+# Получаем нужные данные
+result = {
+    'Тип': pokemon_info['types'][0]['type']['name'],
+    'Вес': pokemon_info['weight'],
+    'Рост': pokemon_info['height'],
+    'Способности': pokemon_info['abilities'][0]['ability']['name'],
+}
+
+print('\n'.join([f'{key}: {value}' for key, value in result.items()]))
